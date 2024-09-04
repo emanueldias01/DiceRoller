de931faeeae327e73934bf83738df4a4043d98e4
sabrinadamascenodev <emanueldias01dev@gmail.com>
2024-09-04 16:32:57 -0300

    melhora posicao de elementos

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -17,7 +19,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,8 +50,15 @@ fun AllScreen(modifier: Modifier = Modifier){
         ) {
         Image(painter = painterResource(id = R.drawable.dice_1), contentDescription = value.toString())
 
+        Spacer(modifier = Modifier.height(35.dp))
+
         Button(onClick = { /*TODO*/ }) {
-            Text(text = stringResource(id = R.string.roll))
+            Text(
+                text = stringResource(id = R.string.roll),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(end = 10.dp, start = 10.dp)
+                )
         }
     }
 }
