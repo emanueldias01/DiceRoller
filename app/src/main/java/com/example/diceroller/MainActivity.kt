d13b7ba25c36a2a3c78ad5c341a99e0049f9d094
sabrinadamascenodev <emanueldias01dev@gmail.com>
2024-09-04 16:29:55 -0300

    adicao da imagem e do botao

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
@@ -30,12 +35,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AllScreen(modifier: Modifier = Modifier){
+
+    val value = 1
     Column(
         modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
         ) {
+        Image(painter = painterResource(id = R.drawable.dice_1), contentDescription = value.toString())
 
-
-
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(id = R.string.roll))
+        }
     }
 }
 
