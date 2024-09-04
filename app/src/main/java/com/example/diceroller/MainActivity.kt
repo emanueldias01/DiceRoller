1d2d8ae7350f6bc73f025e3d0c84f7c91ac29c2a
sabrinadamascenodev <emanueldias01dev@gmail.com>
2024-09-04 16:37:50 -0300

    cria lógica para alterar as imagens de acordo com os resultados

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,14 +12,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +34,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DiceRollerTheme {
-
+                    AllScreen()
                 }
             }
         }
@@ -42,17 +44,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AllScreen(modifier: Modifier = Modifier){
 
-    val value = 1
+    var value by remember { mutableStateOf(0) }
+
+    val imageId = when(value){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
     Column(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
         ) {
-        Image(painter = painterResource(id = R.drawable.dice_1), contentDescription = value.toString())
+        Image(painter = painterResource(id = imageId), contentDescription = value.toString())
 
         Spacer(modifier = Modifier.height(35.dp))
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = { value = (1..6).random() }) {
             Text(
                 text = stringResource(id = R.string.roll),
                 fontSize = 20.sp,
